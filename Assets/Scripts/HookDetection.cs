a0bf151017fc5db725c48b3436f901694f89dbbc
Ashley <ashleyycheemaa@hotmail.com>
2020-04-20 15:25:29 +0100

    Hook goeth and hook returneth

---
+++ Assets/Scripts/HookDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookDetection : PlayerHook
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            hookHit = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHook.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerHook : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hook;
+    [SerializeField]
+    private float hookDistance;
+
+    private bool hasThrown;
+    protected bool hookHit;
+    private Sequence hookTravel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if (!hasThrown)
+            {
+                hasThrown = true;
+                ThrowHook(hasThrown);
+            }
+        }
+
+        //if(hookHit)
+        //{
+        //    hookTravel.Kill();
+        //    hookHit = false;
+        //}
+    }
+
+    private void ThrowHook(bool onHit)
+    {
+        if (onHit)
+        {
+            hookTravel.Append(hook.transform.DOMoveZ(hookDistance, 1.5f).SetEase(Ease.OutSine).OnComplete(() => ThrowHook(false)));
+        }
+        else
+        {
+            hook.transform.DOMoveZ(transform.position.z + 0.5f, 1.5f).SetEase(Ease.OutSine).OnComplete(() => hasThrown = false);
+        }
+    }
+}
